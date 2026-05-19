73fb668be97fe7682a6cbd2424800089f2fd08f2
NeilDMJ <dantemartinez175@gmail.com>
2026-05-18 21:06:00 -0600

    lazypages

--- src/router.tsx
+++ src/router.tsx
@@ -1,35 +1,12 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 import RootLayout from './layout/RootLayout';
 import Reader from './pages/Reader/Reader';
-import IntroSO from './pages/notes/IntroSO';
-import IntroProcesos from './pages/notes/IntroProcesos';
-import CtrlProcesos from './pages/notes/CtrlProcesos';
-import Systemcall from './pages/notes/Systemcall';
-import IdentifyProcess from './pages/notes/IdentifyProcess';
-import callWait from './pages/notes/callWait';
-import Exit from './pages/notes/Exit';
-import Zombies from './pages/notes/Zombies';
-import Hilos from './pages/notes/Hilos';
-import Inanicion from './pages/notes/Inanicion';
-import Prevencion from './pages/notes/Prevencion';
-import Banquero from './pages/notes/Banquero';
-import Deteccion from './pages/notes/Deteccion';
-import Prediccion from './pages/notes/Prediccion';
-import MemoriaIntro from './pages/notes/MemoriaIntro';
-import IntroIPC from './pages/notes/IntroIPC';
-import Pipes from './pages/notes/Pipes';
-import Fifos from './pages/notes/Fifos';
-import Llaves from './pages/notes/Llaves';
-import SemaforosSysV from './pages/notes/SemaforosSysV';
-import MemoriaCompartida from './pages/notes/MemoriaCompartida';
-import ColaMensajes from './pages/notes/ColaMensajes';
-import ComandosIPC from './pages/notes/ComandosIPC';
-import MinishellIntro from './pages/notes/MinishellIntro';
-import MinishellArquitectura from './pages/notes/MinishellArquitectura';
-import MinishellComandosFS from './pages/notes/MinishellComandosFS';
-import MinishellComandosSistema from './pages/notes/MinishellComandosSistema';
 
-
+const lazyPage = (loader: () => Promise<{ default: React.ComponentType }>) =>
+  async () => {
+    const { default: Component } = await loader();
+    return { Component };
+  };
 
 export const router = createBrowserRouter([
   {
@@ -40,33 +17,33 @@ export const router = createBrowserRouter([
         Component: Reader,
         children: [
           { index: true, element: <Navigate to="/introduccion/sistemas-operativos" replace /> },
-          { path: 'introduccion/sistemas-operativos', Component: IntroSO },
-          { path: 'procesos/intro-procesos', Component: IntroProcesos },
-          {path: 'procesos/control-procesos', Component:CtrlProcesos },
-          {path: 'procesos/systemcall-procesos', Component: Systemcall},
-          { path: 'procesos/identify-process', Component: IdentifyProcess},
-          { path: 'procesos/llamada-wait', Component: callWait },
-          { path: 'procesos/llamada-exit', Component: Exit },
-          { path: 'procesos/zombies', Component: Zombies },
-          { path: 'procesos/hilos', Component: Hilos },
-          { path: 'interbloqueo/inanicion', Component: Inanicion },
-          { path: 'interbloqueo/prevencion', Component: Prevencion },
-          { path: 'interbloqueo/banquero', Component: Banquero },
-          { path: 'interbloqueo/deteccion', Component: Deteccion },
-          { path: 'interbloqueo/prediccion', Component: Prediccion },
-          { path: 'memoria/intro', Component: MemoriaIntro },
-          { path: 'ipc/intro-ipc', Component: IntroIPC },
-          { path: 'ipc/pipes', Component: Pipes },
-          { path: 'ipc/fifos', Component: Fifos },
-          { path: 'ipc/llaves', Component: Llaves },
-          { path: 'ipc/semaforos-sysv', Component: SemaforosSysV },
-          { path: 'ipc/memoria-compartida', Component: MemoriaCompartida },
-          { path: 'ipc/cola-mensajes', Component: ColaMensajes },
-          { path: 'ipc/comandos-ipc', Component: ComandosIPC },
-          { path: 'proyecto-minishell/intro', Component: MinishellIntro },
-          { path: 'proyecto-minishell/arquitectura', Component: MinishellArquitectura },
-          { path: 'proyecto-minishell/comandos-fs', Component: MinishellComandosFS },
-          { path: 'proyecto-minishell/comandos-sistema', Component: MinishellComandosSistema },
+          { path: 'introduccion/sistemas-operativos', lazy: lazyPage(() => import('./pages/notes/IntroSO')) },
+          { path: 'procesos/intro-procesos', lazy: lazyPage(() => import('./pages/notes/IntroProcesos')) },
+          { path: 'procesos/control-procesos', lazy: lazyPage(() => import('./pages/notes/CtrlProcesos')) },
+          { path: 'procesos/systemcall-procesos', lazy: lazyPage(() => import('./pages/notes/Systemcall')) },
+          { path: 'procesos/identify-process', lazy: lazyPage(() => import('./pages/notes/IdentifyProcess')) },
+          { path: 'procesos/llamada-wait', lazy: lazyPage(() => import('./pages/notes/callWait')) },
+          { path: 'procesos/llamada-exit', lazy: lazyPage(() => import('./pages/notes/Exit')) },
+          { path: 'procesos/zombies', lazy: lazyPage(() => import('./pages/notes/Zombies')) },
+          { path: 'procesos/hilos', lazy: lazyPage(() => import('./pages/notes/Hilos')) },
+          { path: 'interbloqueo/inanicion', lazy: lazyPage(() => import('./pages/notes/Inanicion')) },
+          { path: 'interbloqueo/prevencion', lazy: lazyPage(() => import('./pages/notes/Prevencion')) },
+          { path: 'interbloqueo/banquero', lazy: lazyPage(() => import('./pages/notes/Banquero')) },
+          { path: 'interbloqueo/deteccion', lazy: lazyPage(() => import('./pages/notes/Deteccion')) },
+          { path: 'interbloqueo/prediccion', lazy: lazyPage(() => import('./pages/notes/Prediccion')) },
+          { path: 'memoria/intro', lazy: lazyPage(() => import('./pages/notes/MemoriaIntro')) },
+          { path: 'ipc/intro-ipc', lazy: lazyPage(() => import('./pages/notes/IntroIPC')) },
+          { path: 'ipc/pipes', lazy: lazyPage(() => import('./pages/notes/Pipes')) },
+          { path: 'ipc/fifos', lazy: lazyPage(() => import('./pages/notes/Fifos')) },
+          { path: 'ipc/llaves', lazy: lazyPage(() => import('./pages/notes/Llaves')) },
+          { path: 'ipc/semaforos-sysv', lazy: lazyPage(() => import('./pages/notes/SemaforosSysV')) },
+          { path: 'ipc/memoria-compartida', lazy: lazyPage(() => import('./pages/notes/MemoriaCompartida')) },
+          { path: 'ipc/cola-mensajes', lazy: lazyPage(() => import('./pages/notes/ColaMensajes')) },
+          { path: 'ipc/comandos-ipc', lazy: lazyPage(() => import('./pages/notes/ComandosIPC')) },
+          { path: 'proyecto-minishell/intro', lazy: lazyPage(() => import('./pages/notes/MinishellIntro')) },
+          { path: 'proyecto-minishell/arquitectura', lazy: lazyPage(() => import('./pages/notes/MinishellArquitectura')) },
+          { path: 'proyecto-minishell/comandos-fs', lazy: lazyPage(() => import('./pages/notes/MinishellComandosFS')) },
+          { path: 'proyecto-minishell/comandos-sistema', lazy: lazyPage(() => import('./pages/notes/MinishellComandosSistema')) },
         ],
       },
     ],
